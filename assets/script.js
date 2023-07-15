ff6937c211e455738492805506befcf551820171
Xiaoran <xc2422@columbia.edu>
2023-07-15 13:13:04 -0700

    check API

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,22 @@
 $("#beginBtn").on("click", function(){
     location.replace("main.html");
 });
+
+
+
+    var requestUrl="https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD,JPY,EUR"
+    fetch(requestUrl)
+        .then(function(response){
+            // var response=!response.ok
+            if (!response.ok){
+                showError();
+            }else{
+     
+                return response.json();    
+            }
+        })
+        .then(function(data){
+            
+            console.log(data)
+           
+    });  
