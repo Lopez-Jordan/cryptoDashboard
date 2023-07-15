a0fcd009121f856d3a0a4463f8d5d9d92f5ff0af
Jordan Lopez <jordanlopezemail@gmail.com>
2023-07-15 14:33:32 -0700

    form updated

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,32 @@
 $("#beginBtn").on("click", function(){
     location.replace("main.html");
 });
-
 $("#surveyForm").on("submit", function(e){
-  e.preventDefault();
+  e.preventDefault(); // still haven't created object yet
+
+  $("#visDivOne").css("display","none");
+  $("#visDivTwo").css("display","none");
+  $("#visDivThree").css("display","none");
+  $("#visDivFour").css("display","none");
+  $("#create").css("display","block");
+  $("#nextOne").css("display","block");
+  $("#nextTwo").css("display","block");
+  $("#nextThree").css("display","block");
 });
 
+$("#create").on("click", function(){
+  $("#visDivOne").css("display","block");
+  $("#create").css("display","none");
+});
+$("#nextOne").on("click",function(){
+  $("#visDivTwo").css("display","block");
+  $("#nextOne").css("display","none");
+});
+$("#nextTwo").on("click",function(){
+  $("#visDivThree").css("display", "block");
+  $("#nextTwo").css("display","none");
+});
+$("#nextThree").on("click",function(){
+  $("#visDivFour").css("display", "block");
+  $("#nextThree").css("display","none");
+});
