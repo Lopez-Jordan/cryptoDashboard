6e49f70b1743a426815ef56b5b539902ac892a68
Jordan Lopez <jordanlopezemail@gmail.com>
2023-07-15 21:20:21 -0700

    beginning form validation

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,50 @@
 $("#beginBtn").on("click", function(){
     location.replace("main.html");
 });
+
+
+function validateForm(data){
+ 
+}
+
 $("#surveyForm").on("submit", function(e){
   e.preventDefault(); // still haven't created object yet
+
+  var coin1Obj = {
+    symbol : $("#coin1").val(),
+    percent: parseInt($("#percent1").val())
+  }
+  var coin2Obj = {
+    symbol : $("#coin2").val(),
+    percent: parseInt($("#percent2").val())
+  }
+  var coin3Obj = {
+    symbol : $("#coin3").val(),
+    percent: parseInt($("#percent3").val())
+  }
+  var coin4Obj = {
+    symbol : $("#coin4").val(),
+    percent: parseInt($("#percent4").val())
+  }
+  var coin5Obj = {
+    symbol : $("#coin5").val(),
+    percent: parseInt($("#percent5").val())
+  }
+
+  var newObj = {
+    userName: $("#inputName").val(),
+    visited: true,
+    level: parseInt($("#inputLevel").val()),
+    totalInvestment : parseInt($("#investment").val()),
+    coins: [coin1Obj, coin2Obj, coin3Obj, coin4Obj, coin5Obj]
+  }
+
+  if (validateForm(newObj)){
+    localStorage.setItem("user", JSON.stringify(newObj)); //not done!!!!
+  }
+    console.log(newObj); // just for tests!!!
+  
+
 
   $("#visDivOne").css("display","none");
   $("#visDivTwo").css("display","none");
@@ -30,3 +72,5 @@ $("#nextThree").on("click",function(){
   $("#visDivFour").css("display", "block");
   $("#nextThree").css("display","none");
 });
+
+
