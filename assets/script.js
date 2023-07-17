8938a314748b1f0442b1e3eb63bb4723b3524b12
JT Reyes <jesustgreyes@gmail.com>
2023-07-16 22:40:46 -0700

    get data from API

--- assets/script.js
+++ assets/script.js
@@ -8,6 +8,23 @@ $("#surveyForm").on("submit", function(e){
 
 var ti = localStorage.getItem("totalInvestment");
 
+var requestUrl="https://min-api.cryptocompare.com/data/v2/histoday?fsym=BTC&tsym=USD&limit=10"
+  fetch(requestUrl)
+    .then(function(response){
+      // var response=!response.ok
+      if (!response.ok){
+          showError();
+      }else{
+
+          return response.json();    
+      }
+    })
+    .then(function(data){
+        
+      console.log(data)
+        
+  });  
+
 function getTotalInvestment() {
   document.getElementById("totalInvestment").textContent = ti;
 }
