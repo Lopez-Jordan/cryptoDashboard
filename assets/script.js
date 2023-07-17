4c7da5e7f8197a2259435d14f6e97d3aa926bad0
JT Reyes <jesustgreyes@gmail.com>
2023-07-17 01:05:55 -0700

    finsih render functions

--- assets/script.js
+++ assets/script.js
@@ -6,8 +6,18 @@ $("#surveyForm").on("submit", function(e){
   e.preventDefault();
 });
 
+
 var totalInv = localStorage.getItem("totalInvestment");
 var coins = localStorage.getItem("coins");
+var numCoins = coins.length;
+
+function displayDashboard() {
+  for (i=0; i<numCoins; i++) {
+    renderTotalInvestment();
+    renderNetWorth(coins[i].symbol);
+    renderProfit(coins[i].symbol);
+  }
+}
 
 function renderTotalInvestment() {
   document.getElementById("totalInvest").textContent = totalInv;
@@ -20,13 +30,20 @@ function renderNetWorth(name) {
       return response.json();
     })
     .then(function(data) {
-      
-    }
-  });
+      var open = data.Data[0].open;
+      var close = data.Data[10].close;
+      var netWorth = 0;
+      for (i=0; i<numCoins; i++) {
+        var perc = (coins[i].percent)/100;
+        var coinInv = totalInv * perc;
+        var coinClose = (coinInv * close)/open;
+        netWorth += coinClose;
+      }
+      document.getElementById("netWorth").textContent = netWorth;
+    });
+  }
 
 function renderProfit(name) {
-  var numCoins = coins.length;
-  var profit;
   var url = "https://min-api.cryptocompare.com/data/v2/histoday?fsym=" + name + "&tsym=USD&limit=10";
   fetch(url)
     .then(function(response) {
@@ -34,12 +51,15 @@ function renderProfit(name) {
     })
     .then(function(data) {
       var open = data.Data[0].open;
-      var close = data.Data[0].close;
+      var close = data.Data[10].close;
+      var totProfit = 0;
       for (i=0; i<numCoins; i++) {
         var perc = (coins[i].percent)/100;
         var coinInv = totalInv * perc;
-        
-        profit += coinProfit;
+        var coinClose = (coinInv * close)/open;
+        var coinProfit = coinClose - coinInv;
+        totProfit += coinProfit;
     }
+      document.getElementById("profit").textContent = totProfit;
   });
 }
